65e7429560a194072cf2a23369e37b04d195f7e7
Nangman <ckdgh930314@naver.com>
2020-02-15 14:29:02 +0900

    #1.1GET_MOVIES Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,23 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+  {
+    movies(rating: 8.5, limit: 40) {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(loading, error, data);
+  if (loading) {
+    return "loading...";
+  }
+  if (data && data.movies) {
+    return data.movies.map(m => <h1>{m.id}</h1>);
+  }
+};
